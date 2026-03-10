27f1e723e5830dd9b06b99909ab13df4fd2e0f46
larissaolimpio <lrsolimpio@gmail.com>
2026-03-10 17:07:43 -0300

    Create: postForm component

---
+++ src/components/postForm.tsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+
+interface PostData {
+  title: string;
+  content: string;
+}
+
+export default function PostForm() {
+  const [formData, setFormData] = useState<PostData>({
+    title: "",
+    content: "",
+  });
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const isButtonDisabled = !formData.title.trim() || !formData.content.trim();
+  return (
+    <section className="flex flex-col gap-6 rounded-2xl border border-[#CCCCCC] p-6">
+      <h2 className="text-[22px] font-bold text-black">What’s on your mind?</h2>
+
+      <div className="flex flex-col gap-2">
+        <label className="text-[16px] text-black">Title</label>
+        <input
+          placeholder="Hello world"
+          className="w-full rounded-lg border border-[#777777] p-2 focus:outline-[#7695EC]"
+          name="title"
+          value={formData.title}
+          onChange={handleChange}
+        />
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <label className="text-[16px] text-black">Content</label>
+        <textarea
+          placeholder="Content here"
+          rows={3}
+          className="w-full resize-none rounded-lg border border-[#777777] p-2 focus:outline-[#7695EC]"
+          name="content"
+          value={formData.content}
+          onChange={handleChange}
+        />
+      </div>
+
+      <div className="flex justify-end">
+        <button
+          disabled={isButtonDisabled}
+          className="rounded-lg bg-[#7695EC] px-8 py-1.5 font-bold text-white hover:bg-[#5a78d1] disabled:opacity-50"
+        >
+          Create
+        </button>
+      </div>
+    </section>
+  );
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
+import PostForm from "../components/postForm";
 
 export const Route = createFileRoute("/")({
   component: RouteComponent,
@@ -6,8 +7,16 @@ export const Route = createFileRoute("/")({
 
 function RouteComponent() {
   return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
+    <div className="flex min-h-screen justify-center bg-[#DDDDDD]">
+      <div className="min-h-screen w-full max-w-200 bg-white shadow-lg">
+        <header className="flex h-20 items-center bg-[#7695EC] p-6">
+          <h1 className="text-[22px] font-bold text-white">CodeLeap Network</h1>
+        </header>
+
+        <main className="flex flex-col gap-6 p-6">
+          <PostForm />
+        </main>
+      </div>
     </div>
   );
 }
